fb4f4cccac7cd2bcd5e43b36c4297497a4d10de8
Lia Maccapani <73106505+liamaccapani@users.noreply.github.com>
2021-06-29 16:46:34 +0200

    Added text area + ex 9 JS

--- script.js
+++ script.js
@@ -7,3 +7,10 @@ let changeTitle = function() {
 let nightMode = function() {
   let newBackground = document.body.style.backgroundColor = "black"
 }
+
+let changeAddress = function () {
+  let newAddress = document.getElementsByTagName('footer')
+  newAddress[0].innerHTML = 'Second star to the right, straight on till morning'
+  return newAddress
+
+}
