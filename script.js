f45a293bddc4e007b02f9526f058105c8523c513
Lia Maccapani <73106505+liamaccapani@users.noreply.github.com>
2021-07-03 18:47:21 +0200

    Removed instructions file

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+/* This additional exercises, from 7 to 12, use JavaScript as well
+
+EX7.: Write a function to change the h1 text
+
+EX8.: Write a function to change the page background color
+
+EX9.: Write a function to change the footer address with a fake one
+
+EX10.: Write a function to add a CSS class to every Amazon link
+
+EX11: Write a function to toggle a CSS class for all the images in the table; that class should set the visibility of the image
+
+EX12: Write a function to color the price of each product in a different one every time it's invoked */
+
 let changeTitle = function() {
   let newTitle = document.getElementsByTagName('h1')
   newTitle[0].innerHTML = 'Better call Saul'
@@ -15,7 +29,7 @@ let changeAddress = function () {
 
 }
 
-let createAllClasses = function {
+let createAllClasses = function () {
   let parentNode = document.getElementsByTagName('ul')
   let allLinks = parentNode.quesrySelectorAll('li')
   allLinks.classList.add('amazonLink') //not finished
