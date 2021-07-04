845e42115e6187e42c78da863fbfdb8c157cc85e
Lia Maccapani <73106505+liamaccapani@users.noreply.github.com>
2021-07-04 18:57:26 +0200

    Fixed style, night mode and title

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 /* This additional exercises, from 7 to 12, use JavaScript as well
 
-EX7.: Write a function to change the h1 text
+  EX7.: Write a function to change the h1 text
 
-EX8.: Write a function to change the page background color
+  EX8.: Write a function to change the page background color
 
-EX9.: Write a function to change the footer address with a fake one
+  EX9.: Write a function to change the footer address with a fake one
 
 EX10.: Write a function to add a CSS class to every Amazon link
 
@@ -13,24 +13,37 @@ EX11: Write a function to toggle a CSS class for all the images in the table; th
 EX12: Write a function to color the price of each product in a different one every time it's invoked */
 
 let changeTitle = function() {
-  let newTitle = document.getElementsByTagName('h1')
-  newTitle[0].innerHTML = 'Better call Saul'
+  let newTitle = document.querySelector('h1')
+  newTitle.classList.toggle("selected")
+  if (newTitle.innerHTML === "Better not call"){
+    newTitle.innerHTML = "Better call Saul"
+  } else {
+    newTitle.innerHTML = "Better not call"
+  }
   return newTitle
 }
 
-let nightMode = function() {
-  let newBackground = document.body.style.backgroundColor = "black"
+let setNightMode = function(){
+  let nightModeBody = document.body
+  nightModeBody.classList.toggle("dark-mode")
 }
+
 
 let changeAddress = function () {
   let newAddress = document.getElementsByTagName('footer')
   newAddress[0].innerHTML = 'Second star to the right, straight on till morning'
   return newAddress
-
 }
 
-let createAllClasses = function () {
-  let parentNode = document.getElementsByTagName('ul')
-  let allLinks = parentNode.quesrySelectorAll('li')
-  allLinks.classList.add('amazonLink') //not finished
+// let createAllClasses = function (event) {
+//   let allLinks = document.quesrySelectorAll('ul li')
+//   allLinks.classList.add('link-selected')
+//   return allLinks
+// }
+
+let hidePic = function(){
+  let picToHide = document.getElementsByTagName('img')
+  for (i=0; i<picToHide.length; i++){
+    picToHide.classList.toggle('hidden')
+  }
 }
