77a585a98ede0fef14bb093c0af421def71a7f24
liamaccapani <lia.maccapani@gmail.com>
2021-08-03 19:32:53 +0200

    pics visibility

--- script.js
+++ script.js
@@ -8,11 +8,11 @@
 
 EX10.: Write a function to add a CSS class to every Amazon link
 
-EX11: Write a function to toggle a CSS class for all the images in the table; that class should set the visibility of the image
+  EX11: Write a function to toggle a CSS class for all the images in the table; that class should set the visibility of the image
 
 EX12: Write a function to color the price of each product in a different one every time it's invoked */
 
-let changeTitle = function() {
+const changeTitle = function() {
   let newTitle = document.querySelector('h1')
   newTitle.classList.toggle("selected")
   if (newTitle.innerHTML === "Better not call"){
@@ -23,13 +23,13 @@ let changeTitle = function() {
   return newTitle
 }
 
-let setNightMode = function(){
+const setNightMode = function(){
   let nightModeBody = document.body
   nightModeBody.classList.toggle("dark-mode")
 }
 
 
-let changeAddress = function () {
+const changeAddress = function () {
   let newAddress = document.getElementsByTagName('footer')
   newAddress[0].innerHTML = 'Second star to the right, straight on till morning'
   return newAddress
@@ -41,9 +41,28 @@ let changeAddress = function () {
 //   return allLinks
 // }
 
-let hidePic = function(){
-  let picToHide = document.getElementsByTagName('img')
-  for (i=0; i<picToHide.length; i++){
-    picToHide.classList.toggle('hidden')
+// const hidePic = function(){
+//   let picToHide = document.getElementsByTagName('img')
+//   //console.log(picToHide)
+//   for (i=0; i<picToHide.length; i++){
+//     picToHide[i].addEventListener('click', function(){
+//       picToHide[i].classList.toggle('hidden')
+//     })
+//   }
+// }
+
+// hidePic()
+
+const hidePic = function(){
+  let imgs = document.querySelectorAll('img')
+  for (let img of imgs){
+    img.addEventListener('click', function(){
+      img.classList.toggle('hidden')
+      console.log(img)
+    })
   }
+
 }
+
+hidePic()
+//pics are hidden, find a way to toggle them back
