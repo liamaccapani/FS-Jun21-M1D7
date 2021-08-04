586c46d0710c84042b38e0ee85261c2a82aa7819
liamaccapani <lia.maccapani@gmail.com>
2021-08-04 10:53:07 +0200

    random colors

--- script.js
+++ script.js
@@ -6,12 +6,18 @@
 
   EX9.: Write a function to change the footer address with a fake one
 
-EX10.: Write a function to add a CSS class to every Amazon link
+  EX10.: Write a function to add a CSS class to every Amazon link
 
   EX11: Write a function to toggle a CSS class for all the images in the table; that class should set the visibility of the image
 
 EX12: Write a function to color the price of each product in a different one every time it's invoked */
 
+window.onload = function(){
+  addClassToLinks()
+}
+
+
+// #7
 const changeTitle = function() {
   let newTitle = document.querySelector('h1')
   newTitle.classList.toggle("selected")
@@ -23,46 +29,106 @@ const changeTitle = function() {
   return newTitle
 }
 
+
+// #8 
 const setNightMode = function(){
   let nightModeBody = document.body
   nightModeBody.classList.toggle("dark-mode")
 }
 
 
+// #9
 const changeAddress = function () {
   let newAddress = document.getElementsByTagName('footer')
   newAddress[0].innerHTML = 'Second star to the right, straight on till morning'
   return newAddress
 }
 
-// let createAllClasses = function (event) {
-//   let allLinks = document.quesrySelectorAll('ul li')
-//   allLinks.classList.add('link-selected')
-//   return allLinks
-// }
 
+// #10-a
+// const addClassToLinks = function(){
+//   let links = document.getElementsByTagName('a')
+//     console.log(links)
+//     for(let link of links){
+//       link.addEventListener('mouseover', function(){
+//         link.classList.add('amazon-link')
+//         console.log(link)
+//       })
+//     }
+// }
+// addClassToLinks()
+
+const addClassToLinks = function(){
+  let links = document.querySelectorAll('ul li a')
+    console.log(links)
+    links.forEach(link => {
+      link.classList.add('amazon-link')
+      console.log(link)
+    });
+}
+//called it in window.onload
+
+// #11-a
 // const hidePic = function(){
 //   let picToHide = document.getElementsByTagName('img')
 //   //console.log(picToHide)
 //   for (i=0; i<picToHide.length; i++){
-//     picToHide[i].addEventListener('click', function(){
-//       picToHide[i].classList.toggle('hidden')
+//     //'can't toggle class of undefined' -> needs to be stored -> currentImg
+//     let currentImg = picToHide[i]
+//     currentImg.addEventListener('click', function(){
+//       currentImg.classList.toggle('hidden')
+//       console.log(currentImg)
+//     })
+//   }
+// }
+// hidePic()
+
+// #11-b
+// const hidePic = function(){
+//   let imgs = document.querySelectorAll('img')
+//   for (let img of imgs){
+//     img.addEventListener('click', function(){
+//       img.classList.toggle('hidden')
+//       console.log(img)
 //     })
 //   }
 // }
 
 // hidePic()
 
-const hidePic = function(){
-  let imgs = document.querySelectorAll('img')
-  for (let img of imgs){
-    img.addEventListener('click', function(){
-      img.classList.toggle('hidden')
-      console.log(img)
+
+// #11-c
+// const hidePic = function(){
+//   let imgs = document.querySelectorAll('img')
+//     imgs.forEach(img => {
+//       img.addEventListener('click', function(){
+//         img.classList.toggle('hidden')
+//         console.log(img)
+//     })
+//   })
+// }
+
+// hidePic()
+
+// 💥 pics are hidden, find a way to toggle them back
+
+
+// #12
+const colorGenerator = function(){
+  let randomColor = '#' + Math.floor(Math.random()*16777215).toString(16)
+  return randomColor
+}
+/* Random Color Explanation:
+  https://dev.to/akhil_001/generating-random-color-with-single-line-of-js-code-fhj
+*/
+
+const colorPrice = function(){
+  let pricesTd = document.querySelectorAll('table tr td:nth-child(4)')
+  for (let priceTd of pricesTd){
+    priceTd.addEventListener('mouseover', function(){
+      priceTd.style.color = colorGenerator()
     })
   }
-
 }
 
-hidePic()
-//pics are hidden, find a way to toggle them back
+colorPrice()
